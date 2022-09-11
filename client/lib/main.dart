43562da5a2ba4e85841eb0f55ae224ec7d77f30f
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-09-11 12:44:59 +0530

    Created splash static page which will be there before initialization

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,28 +1,16 @@
-import 'package:client/pages/home_page.dart';
+import 'package:client/pages/main_page.dart';
+import 'package:client/pages/splash_page.dart';
 import 'package:client/provider/movie_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    SplashPage(
+      key: UniqueKey(),
+      onInitializationComplete: () => null,
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: ((context) => MovieProvider()),
-        ),
-      ],
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: HomePage(),
-      ),
-    );
-  }
-}
+// Pages
---
+++ client/lib/pages/splash_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  final VoidCallback onInitializationComplete;
+  const SplashPage({required Key key, required this.onInitializationComplete})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SplashPageState();
+  }
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "BingStar",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.contain,
+                  image: AssetImage("assets/images/logo.png"))),
+        ),
+      ),
+    );
+  }
+}
